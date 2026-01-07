18b801674e33a0d9e7295d3829fbf8332a98fdb8
sunnyGupta128 <sunnygupta6497@gmail.com>
2026-01-08 03:29:29 +0530

    refactor: update linting configuration and remove unused CSS
    - Removed stylelint from scripts and deleted style.css file.
    - Updated linting scripts to exclude CSS linting.
    - Changed stylelint configuration to extend stylelint-config-standard-scss.
    - Added stylelint-config-standard-scss as a dependency.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,4 @@
 /** @type {import('stylelint').Config} */
-module.exports = {
-  extends: ["stylelint-config-standard"],
-  rules: {
-    "selector-class-pattern": null,
-    "custom-property-pattern": null,
-  },
+export default {
+  extends: ["stylelint-config-standard-scss"],
 };
