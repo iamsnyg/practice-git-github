063ed8a1a7a376caa9c772103e2ec6c35752046e
sunnyGupta128 <sunnygupta6497@gmail.com>
2026-01-08 02:32:49 +0530

    Fix stylelint configuration - migrate to stylelint.config.js

---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: "stylelint-config-standard",
+  rules: {
+    "selector-class-pattern": null,
+    "custom-property-pattern": null
+  }
+};
