612c44b84f6fe14d6fba2f74b2c8f88456cf8b94
sunnyGupta128 <sunnygupta6497@gmail.com>
2026-01-07 22:41:40 +0530

    random

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// button color change on click
+document.getElementById("myButton").addEventListener("click", function() {
+    this.style.backgroundColor = "#007BFF";
+    this.style.color = "white";
+    this.textContent = "Clicked!";
+});
