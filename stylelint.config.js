f03b539c91e01ec65902ec065cff0d0adadb58b0
sunnyGupta128 <sunnygupta6497@gmail.com>
2026-01-08 02:37:41 +0530

    Update stylelint config extends to use array syntax

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,6 @@
 /** @type {import('stylelint').Config} */
 module.exports = {
-    extends: "stylelint-config-standard",
+    extends: ["stylelint-config-standard"],
     rules: {
         "selector-class-pattern": null,
         "custom-property-pattern": null,
